a9f319e66fe9571116a36b7ca889dbfaeb02ebe0
DJ Meph <norm.witte@gmail.com>
2022-05-30 04:51:09 +0000

    Setup home screen web app

--- apps/digital-www/pages/_app.tsx
+++ apps/digital-www/pages/_app.tsx
@@ -28,6 +28,41 @@ function CustomApp({ Component, pageProps }: AppProps) {
           <FavoritesProvider>
             <Head>
               <title>Lakes of Fire 2022 Digital WWW</title>
+              <link rel="icon" type="image/x-icon" href="favicon.ico" />
+              <link rel="shortcut icon" href="favicon.ico" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+              />
+              <link rel="manifest" href="/site.webmanifest" />
+              <link
+                rel="mask-icon"
+                href="/safari-pinned-tab.svg"
+                color="#5bbad5"
+              />
+              <meta name="format-detection" content="telephone=no" />
+              <meta name="msapplication-tap-highlight" content="no" />
+              <meta name="msapplication-TileColor" content="#ffffff" />
+              <meta name="theme-color" content="#ffffff" />
+              <meta name="apple-mobile-web-app-capable" content="yes" />
+              <meta
+                name="apple-mobile-web-app-status-bar-style"
+                content="white"
+              />
+              <meta name="msapplication-config" content="/browserconfig.xml" />
             </Head>
             <NavbarComponent />
             <div className={styles['page-buffer']}>
