c6c139ba94f3acddd13ff07cb579d43239fd0d2f
Ross Smith <r.t.smith.1984@gmail.com>
2022-06-09 22:34:01 +0000

    Added vehicles page; styled vehicles component

---
+++ apps/digital-www/pages/vehicles/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+
+import { VehiclesComponent } from '@lof-digital-www/www-events';
+
+export function Vehicles() {
+  return (
+    <Container>
+      <VehiclesComponent />
+    </Container>
+  );
+}
+
+export default Vehicles;
