3f9b92d52efa7fc77d99cf50440e154aa2675193
Ross Smith <r.t.smith.1984@gmail.com>
2022-06-10 12:32:20 +0000

    Banner and VehiclesComponent styling

--- apps/digital-www/pages/vehicles/index.tsx
+++ apps/digital-www/pages/vehicles/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from 'react-bootstrap';
 
+import { Banner } from '@lof-digital-www/shared';
 import { VehiclesComponent } from '@lof-digital-www/www-events';
 
 export function Vehicles() {
   return (
-    <Container>
+    <Container fluid className="px-0">
+      <Banner title="Mutant Vehicles" />
       <VehiclesComponent />
     </Container>
   );
