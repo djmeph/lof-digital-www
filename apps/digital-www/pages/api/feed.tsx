ca03c0b6ea11a959d1a2d3039ba3d7e9f0225f8a
DJ Meph <info@djmeph.net>
2024-04-10 05:58:36 +0000

    2024 dummy data

--- apps/digital-www/pages/api/feed.tsx
+++ apps/digital-www/pages/api/feed.tsx
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  const data = await fetch('https://whatwherewhen.lakesoffire.org/events.json');
+  // const data = await fetch('https://whatwherewhen.lakesoffire.org/events.json');
+  const data = await fetch('http://localhost:4200/events.json');
   const json = await data.json();
   res.json(json.coalesce);
 }
--- libs/www-events/src/interfaces/www-events.enum.ts
+++ libs/www-events/src/interfaces/www-events.enum.ts
@@ -9,13 +9,13 @@ export enum DayOfWeek {
 }
 
 export const datesOfWeek = {
-  [DayOfWeek.MONDAY]: '2023-07-10',
-  [DayOfWeek.TUESDAY]: '2023-07-11',
-  [DayOfWeek.WEDNESDAY]: '2023-07-12',
-  [DayOfWeek.THURSDAY]: '2023-07-13',
-  [DayOfWeek.FRIDAY]: '2023-07-14',
-  [DayOfWeek.SATURDAY]: '2023-07-15',
-  [DayOfWeek.SUNDAY]: '2023-07-16',
+  [DayOfWeek.MONDAY]: '2024-07-15',
+  [DayOfWeek.TUESDAY]: '2024-07-16',
+  [DayOfWeek.WEDNESDAY]: '2024-07-17',
+  [DayOfWeek.THURSDAY]: '2024-07-18',
+  [DayOfWeek.FRIDAY]: '2024-07-19',
+  [DayOfWeek.SATURDAY]: '2024-07-20',
+  [DayOfWeek.SUNDAY]: '2024-07-21',
 };
 
 export enum EventRecurrance {
